266df98b0fc059780ad4f00e16d8e290735a134c
gnauheinna <116969903+gnauheinna@users.noreply.github.com>
2024-02-22 22:32:54 -0500

    updated app.json attempt to fix update

---
+++ styles/ProfileStyles 2.ts
@@ -0,0 +1,46 @@
+/* fonts */
+export const FontFamily = {
+  m3TitleSmall: "Roboto-Medium",
+  m3BodyMedium: "Roboto-Regular",
+  interRegular: "Inter-Regular",
+  interMedium: "Inter-Medium",
+  stolzl: "Stolzl",
+  interLight: "Inter-Light",
+};
+/* font sizes */
+export const FontSize = {
+  m3BodyMedium_size: 14,
+  size_mini_8: 15,
+  size_sm_9: 14,
+  size_mini: 15,
+  size_smi_5: 13,
+};
+/* Colors */
+export const Color = {
+  colorWhite: "#fff",
+  m3SysLightSurface: "#fef7ff",
+  m3SysLightSurfaceContainer: "#f3edf7",
+  m3SysLightPrimary: "#6750a4",
+  m3SysLightOnSurfaceVariant: "#49454f",
+  colorDarkslategray_100: "#453b4f",
+  m3Black: "#000",
+  colorMediumpurple_100: "rgba(120, 93, 189, 0.18)",
+  colorGray_100: "#818181",
+  colorCornflowerblue: "#00a3ff",
+};
+/* Paddings */
+export const Padding = {
+  p_xl: 20,
+  p_5xs: 8,
+  p_xs: 12,
+  p_3xs: 10,
+  p_base: 16,
+  p_9xs: 4,
+};
+/* border radiuses */
+export const Border = {
+  br_xs: 12,
+  br_81xl: 100,
+  br_22xl_4: 41,
+  br_23xl_6: 43,
+};
