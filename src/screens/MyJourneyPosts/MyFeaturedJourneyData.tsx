89b1440afccfbd99e6aa1fce6adf5499fd452b88
aonnadi <aninja1008@gmail.com>
2024-02-25 09:12:33 -0500

    define structure for featured journeys; need to transfer info

--- src/screens/MyJourneyPosts/MyFeaturedJourneyData.tsx
+++ src/screens/MyJourneyPosts/MyFeaturedJourneyData.tsx
@@ -44,6 +44,7 @@ export const FeatJourneys: FeaturedJourney = {
             date: "Dec 10th 2023",
             photoName: "bailey",
         },
+    
         question: {
             q1: "Q: When did you start to be aware of your first-gen identity?" +
             "What does that mean to you?",
@@ -109,13 +110,33 @@ export const FeatJourneys: FeaturedJourney = {
     },
     // Nana
     '3' : {
-        user : {
+        author : {
             journeyTitle: "Voice of a First-Gen Graduate, Entrepreneur, Faculty",
             authorName: "Nana Younge",
             journeyID: "XlT9K5adSYcud8VOybpKjQL0wHrR5og4",
-            intro: "Founder of Get Girls Going Program Director at Innovate@BU",},
-        header: {
-            heading: "I’ve learned a lot in my career, but these are the most important lessons I’ve learned.",
+            intro: "Founder of Get Girls Going Program Director at Innovate@BU",
+            date: "Dec 4th 2023",
+            photoName: "nana",
+        },
+        question: {
+            q1: "Q: What does first-gen mean to you?",
+            q2: "Q: How does your first gen identity make you who you are today?",
+            q3: "Q: What factors do you believe contributed most to your success?",
+            q4: "Q: What are your top 3 pieces of advice for our students?",
+            q5: "",
+            q6: "",
+            q7: "",
+            q8: "",
+        },
+        answer: {
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            a5: "",
+            a6: "",
+            a7: "",
+            a8: "",
         },
     },
    
@@ -126,57 +147,28 @@ export const FeatJourneys: FeaturedJourney = {
             authorName: "Shateva Long",
             journeyID: "qwrr4XlT9K5adSasdasferqL0wHrR5og4",
             intro: "BU Alumni Product Manager @Microsoft",
-            date: "Nov 6th 2023",
+            date: "Nov 2nd 2023",
             photoName: "shateva",
         },
-        header: {
-            heading: "I’ve learned a lot in my career, but these are the most important lessons I’ve learned.",
+        question: {
+            q1: "Q: What does first-gen mean to you?",
+            q2: "Q: How does your first gen identity make you who you are today?",
+            q3: "Q: What factors do you believe contributed most to your success?",
+            q4: "Q: What are your top 3 pieces of advice for our students?",
+            q5: "",
+            q6: "",
+            q7: "",
+            q8: "",
         },
-        process: {
-            step1: "1. You can visit Terrier Central to learn more about the hundreds of clubs on campus.",
-            step2: "2. Get in touch with fellow students in class and see what they may be involved in.",
-            step3: "3. Visit Splash that happens at the start of every semester.",
-            step4: "",
+        answer: {
+            a1: "",
+            a2: "",
+            a3: "",
+            a4: "",
+            a5: "",
+            a6: "",
+            a7: "",
+            a8: "",
         },
-        additionalInfo: {
-            info1: "",
-            info2: "",
-            info3: "",
-            info4: "",
-        },
-        challenges: {
-            challenge1: "• I've felt that the multitude of clubs and organizations can be overwhelming, and there's the discomfort of trying something new without knowing anyone in the group you want to join.", 
-            challenge2: "• I've learned that BU groups are very welcoming to all individuals of any background. So don’t feel alone, there will always be someone that wants to talk and help you feel comfortable.",
-            challenge3: "",
-        },
-        takeaways: {
-            takeaway1: "You have plenty of time to get involved with a community outside of your classroom. Especially throughout the semester, people are always having events and willing to welcome new members :)",
-            takeaway2: "You also never have to stick with the organization if you don’t like it. From personal experience, I’ve found it helpful to try a lot of things and reflect on whether I like or don’t like something.",
-            takeaway3: "",
-        },
-        resources: {
-            resource1: "Make sure you click to learn more about Diversity and Inclusion at BU",
-            resource2: "Click here to view all BU clubs and organizations",
-            resource3: "",
-        },
-        experience: {
-            experience1: '',
-        },
-        additionalGroups: {
-            group1: "Center for Diversity, Equity, & Inclusion (DEI), Questrom School of Business",
-            group2: "The Newbury Center",
-            group3: "Howard Thurman Center",
-        },
-        groupInfo: {
-            groupInfo1: "The center provides diversity education, programs, and community building initiatives for all Questrom students, both undergraduate and graduate.",
-            groupInfo2: "Supporting and celebrating first-generation undergraduate, graduate, and professional students at BU.",
-            groupInfo3: "This is a place where cultural expression in all of its forms is embraced and encouraged.",
-        },
-        links: {
-            link1: '',
-            link2: "https://www.bu.edu/sao/get-involved/student-organizations/",
-            link3: "",
-        
-        }
     },
 }
